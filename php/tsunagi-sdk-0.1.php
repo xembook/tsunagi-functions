2a3633ed8a5d82c5520207b4da4f183a08d2c03a
xembook <mediaprogramer+xembok@gmail.com>
2022-08-22 23:09:58 +0900

    Update tsunagi-sdk-0.1.php

--- php/tsunagi-sdk-0.1.php
+++ php/tsunagi-sdk-0.1.php
@@ -5,6 +5,76 @@ function restaurant_check($meal, $tax, $tip) {
     $total_amount = $meal + $tax_amount + $tip_amount;
     return $total_amount;
 }
+
+
+
+function load_catjson() {
+    return 0;
+}
+
+function load_layout() {
+    return 0;
+}
+
+function to_camel_case() {
+    return 0;
+}
+
+function prepare_transaction() {
+    return 0;
+}
+
+function prepare_transaction() {
+    return 0;
+}
+
+function parse_transaction() {
+    return 0;
+}
+
+function build_transaction() {
+    return 0;
+}
+
+function get_verifiable_data() {
+    return 0;
+}
+
+
+function hash_transaction() {
+    return 0;
+}
+
+function update_transaction() {
+    return 0;
+}
+
+function count_size() {
+    return 0;
+}
+
+function hexlify_transaction() {
+    return 0;
+}
+
+function sign_transaction() {
+    return 0;
+}
+
+function cosign_transaction() {
+    return 0;
+}
+
+function generate_address_alias_id() {
+    return 0;
+}
+
+function generate_address_id() {
+    return 0;
+}
+
+
+
 ?>
 
 
