57b3ef665e7f136886d183149450155a147d45ac
xembook <mediaprogramer+xembok@gmail.com>
2022-08-24 18:47:53 +0900

    php update

--- php/test_0_1.php
+++ php/test_0_1.php
@@ -1,8 +1,13 @@
 <?php
 
 include 'tsunagi-sdk-0.1.php';
+require 'vendor/autoload.php';
+use Base32\Base32;
 
 class test_0_1 extends \PHPUnit\Framework\TestCase {
+
+
+
 
     public function testWithTaxAndTip() {
         $meal = 100;
@@ -11,6 +16,55 @@ class test_0_1 extends \PHPUnit\Framework\TestCase {
         $result = restaurant_check($meal, $tax, $tip);
         $this->assertEquals(130, $result);
     }
+
+    public function testWithTaxAndTip2() {
+
+		$network = [
+			"version" => 1,
+			"network" => "TESTNET",
+			"generationHash" => "7fccd304802016bebbcd342a332f91ff1f3bb5e902988b352697be245f48e836",
+			"currencyMosaicId" => 0x3A8416DB2D53B6C8,
+			"currencyNamespaceId" => 0xE74B99BA41F4AFEE,
+			"currencyDivisibility" => 6,
+			"epochAdjustment" => 1637848847,
+			"catjasonBase" => "https://xembook.github.io/tsunagi-sdk/catjson/",
+			"wellknownNodes" => [
+				"https://sym-test.opening-line.jp:3001",
+				"https://sym-test.opening-line.jp:3001",
+				"https://sym-test.opening-line.jp:3001",
+			]
+		];
+
+
+		//Alice->Bob
+		$tx1 = [
+			"type" => "TRANSFER",
+			"signer_public_key" => "5f594dfc018578662e0b5a2f5f83ecfb1cda2b32e29ff1d9b2c5e7325c4cf7cb",
+			"fee" => 25000,
+			"deadline" => 0,
+			"recipient_address" => bin2hex(Base32::decode("TCO7HLVDQUX6V7C737BCM3VYJ3MKP6REE2EKROA")),
+			"mosaics" => [
+				["mosaic_id" =>  0x2A09B7F9097934C2, "amount" => 1],
+				["mosaic_id" =>  0x3A8416DB2D53B6C8, "amount" => 100],
+			],
+			"message" => "Hello Tsunagi(Catjson) SDK!",
+
+		];
+
+		var_dump($tx1);
+
+		$catjson = load_catjson($tx1,$network);
+		$layout = load_layout($tx1,$catjson,false); //isEmbedded false
+		var_dump($layout);
+
+        $meal = 100;
+        $tax = 10;
+        $tip = 20;
+        $result = restaurant_check($meal, $tax, $tip);
+        $this->assertEquals(130, $result);
+
+    }
+
 
 }
 ?>
--- php/tsunagi-sdk-0.1.php
+++ php/tsunagi-sdk-0.1.php
@@ -8,25 +8,67 @@ function restaurant_check($meal, $tax, $tip) {
 
 
 
-function load_catjson() {
-    return 0;
+function load_catjson($tx,$network) {
+
+	$jsonFile;
+	if($tx["type"] === "AGGREGATE_COMPLETE" || $tx["type"] === "AGGREGATE_BONDED"){
+		$jsonFile =  "aggregate.json";
+	}else{
+		$jsonFile =  strtolower($tx["type"]) . ".json";
+	}
+
+	$res = file_get_contents($network["catjasonBase"] . $jsonFile);
+	$catjson = json_decode($res,true);
+	
+	return $catjson;
 }
 
-function load_layout() {
-    return 0;
+function load_layout($tx,$catjson,$is_embedded) {
+
+	$prefix;
+	if($is_embedded){
+		$prefix = "Embedded";
+	}else{
+		$prefix = "";
+	}
+
+	$layoutName;
+
+	if(      $tx["type"] === "AGGREGATE_COMPLETE"){ $layoutName = "AggregateCompleteTransaction";
+	}else if($tx["type"] === "AGGREGATE_BONDED"){   $layoutName = "AggregateBondedTransaction";
+	}else{
+		$layoutName = $prefix . to_camel_case(strtolower($tx["type"])) . "Transaction";
+	}
+
+	$conditions = ["prefix" => $prefix,"layoutName" => $layoutName];
+
+//	var_dump($catjson);
+	$factory = array_filter($catjson, function($item,$key)use($conditions){
+		var_dump(isset($item['factory_type']) && $item['factory_type'] == $conditions["prefix"] . "Transaction" && $item["name"] === $conditions["layoutName"]);
+	  return isset($item['factory_type']) && $item['factory_type'] == $conditions["prefix"] . "Transaction" && $item["name"] === $conditions["layoutName"];
+	}, ARRAY_FILTER_USE_BOTH);
+
+//	var_dump($conditions["prefix"] . "Transaction");
+//	var_dump($conditions["layoutName"]);
+	var_dump("========================================");
+	print_r(array_values($factory));
+	
+
+	return array_values($factory)[0]["layout"];
 }
 
-function to_camel_case() {
-    return 0;
+function to_camel_case($str) {
+//    return lcfirst(strtr(ucwords(strtr($str, ['_' => ' '])), [' ' => '']));
+
+	return str_replace(' ', '', ucwords(str_replace('_', ' ', $str)));
+
 }
 
 function prepare_transaction() {
     return 0;
 }
 
-function prepare_transaction() {
-    return 0;
-}
+
 
 function parse_transaction() {
     return 0;
