c61dd460663410de51c2488c2c92dac0a054bfef
xembook <mediaprogramer+xembok@gmail.com>
2022-08-26 06:33:05 +0900

    php update

--- php/test_0_1.php
+++ php/test_0_1.php
@@ -51,13 +51,72 @@ class test_0_1 extends \PHPUnit\Framework\TestCase {
 
 		];
 
-		var_dump($tx1);
+//		var_dump($tx1);
 
 		$catjson = load_catjson($tx1,$network);
 		$layout = load_layout($tx1,$catjson,false); //isEmbedded false
-		var_dump($layout);
+//		var_dump($layout);
 		$prepared_tx = prepare_transaction($tx1,$layout,$network); //TX事前準備
+		$parsed_tx = parse_transaction($prepared_tx,$layout,$catjson,$network);
 
+        $meal = 100;
+        $tax = 10;
+        $tip = 20;
+        $result = restaurant_check($meal, $tax, $tip);
+        $this->assertEquals(130, $result);
+
+    }
+
+    public function testWithTaxAndTip3() {
+
+		$network = [
+			"version" => 1,
+			"network" => "TESTNET",
+			"generationHash" => "7fccd304802016bebbcd342a332f91ff1f3bb5e902988b352697be245f48e836",
+			"currencyMosaicId" => 0x3A8416DB2D53B6C8,
+			"currencyNamespaceId" => 0xE74B99BA41F4AFEE,
+			"currencyDivisibility" => 6,
+			"epochAdjustment" => 1637848847,
+			"catjasonBase" => "https://xembook.github.io/tsunagi-sdk/catjson/",
+			"wellknownNodes" => [
+				"https://sym-test.opening-line.jp:3001",
+				"https://sym-test.opening-line.jp:3001",
+				"https://sym-test.opening-line.jp:3001",
+			]
+		];
+
+
+		//Alice->Bob
+		$tx1 = [
+			"type" => "TRANSFER",
+			"signer_public_key" => "5f594dfc018578662e0b5a2f5f83ecfb1cda2b32e29ff1d9b2c5e7325c4cf7cb",
+			"recipient_address" => bin2hex(Base32::decode("TCO7HLVDQUX6V7C737BCM3VYJ3MKP6REE2EKROA")),
+			"mosaics" => [
+				["mosaic_id" =>  0x2A09B7F9097934C2, "amount" => 1],
+				["mosaic_id" =>  0x3A8416DB2D53B6C8, "amount" => 100],
+			],
+			"message" => "Hello Tsunagi(Catjson) SDK!",
+
+		];
+
+		$agg_tx = [
+			"type" => 'AGGREGATE_COMPLETE',
+			"signer_public_key" => "5f594dfc018578662e0b5a2f5f83ecfb1cda2b32e29ff1d9b2c5e7325c4cf7cb",
+			"fee" => 1000000,
+			"deadline" => 0,
+			"transactions" => [$tx1],
+
+		];
+
+
+
+//		var_dump($tx1);
+
+		$catjson = load_catjson($agg_tx,$network);
+		$layout = load_layout($agg_tx,$catjson,false); //isEmbedded false
+//		var_dump($layout);
+		$prepared_tx = prepare_transaction($agg_tx,$layout,$network); //TX事前準備
+		$parsed_tx = parse_transaction($prepared_tx,$layout,$catjson,$network);
 
         $meal = 100;
         $tax = 10;
--- php/tsunagi-sdk-0.1.php
+++ php/tsunagi-sdk-0.1.php
@@ -40,10 +40,9 @@ function load_layout($tx,$catjson,$is_embedded) {
 	}
 
 	$conditions = ["prefix" => $prefix,"layoutName" => $layoutName];
-	$factory = array_filter($catjson, function($item,$key)use($conditions){
-		var_dump(isset($item['factory_type']) && $item['factory_type'] == $conditions["prefix"] . "Transaction" && $item["name"] === $conditions["layoutName"]);
-	  return isset($item['factory_type']) && $item['factory_type'] == $conditions["prefix"] . "Transaction" && $item["name"] === $conditions["layoutName"];
-	}, ARRAY_FILTER_USE_BOTH);
+	$factory = array_filter($catjson, function($item)use($conditions){
+		return isset($item['factory_type']) && $item['factory_type'] == $conditions["prefix"] . "Transaction" && $item["name"] === $conditions["layoutName"];
+	});
 
 	return array_values($factory)[0]["layout"];
 }
@@ -106,10 +105,12 @@ function prepare_transaction($tx,$layout,$network) {
 		$txes = [];
 		foreach($tx["transactions"] as $e_tx){
 
-			$e_catjson = $load_catjson($e_tx,$network);
-			$e_layout = $load_layout($e_tx,$e_catjson,true);
+			print_r($e_tx);
+
+			$e_catjson = load_catjson($e_tx,$network);
+			$e_layout = load_layout($e_tx,$e_catjson,true);
 			//再帰処理
-			$e_prepared_tx = $prepare_transaction($e_tx,$e_layout,$network);
+			$e_prepared_tx = prepare_transaction($e_tx,$e_layout,$network);
 			array_push($txes,$e_prepared_tx);
 		}
 		$prepared_tx["transactions"] = $txes;
@@ -120,8 +121,269 @@ function prepare_transaction($tx,$layout,$network) {
 
 
 
-function parse_transaction() {
-    return 0;
+function parse_transaction($tx,$layout,$catjson,$network) {
+
+print_r("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+	$parsed_tx = []; //return
+	foreach($layout as $layer){
+
+		$layer_type = $layer["type"];
+		$layer_disposition;
+		if(isset($layer["disposition"])){
+			$layer_disposition = $layer["disposition"];
+		}
+		print_r($layer_type . "\n");
+		$catitem = array_filter($catjson, function($cj) use($layer_type){
+			return $cj["name"] === $layer_type;
+		});
+//		$catitem = array_values($filter_item)[0];
+
+		if(isset($layer["condition"])){
+			if($layer["condition_operation"] === "equals"){
+				if($layer["condition_value"] !== $tx[$layer["condition"]]){
+					continue;
+				}
+			}
+		}
+
+		if($layer_disposition === "const"){
+			continue;
+
+		}else if($layer_type === "EmbeddedTransaction"){
+			
+			$tx_layer = $layer;
+			$items = [];
+			foreach($tx["transactions"] as $e_tx){ //小文字のeはembeddedの略
+				$e_catjson = load_catjson($e_tx,$network);//catjsonの更新
+				$e_layout = load_layout($e_tx,$e_catjson,true); //isEmbedded:true
+				$e_parsed_tx = parse_transaction($e_tx,$e_layout,$e_catjson,$network); //再帰
+				array_push($items,$e_parsed_tx);
+			}
+			$tx_layer["layout"] = $items;
+			array_push($parsed_tx,$tx_layer);
+			continue;
+
+		}else if(isset($catitem["layout"]) && isset($tx[$layer["name"]]) ){ // else:byte,struct
+
+			$tx_layer = $layer;
+			$items = [];
+			foreach($tx[$layer["name"]] as $item){
+
+				$filter_layer = array_filter($catjson, function($cj) use($layer_type){
+					return $cj["name"] === $layer_type;
+				});
+
+				$item_parsed_tx = parse_transaction($item,$filter_layer["layout"],$catjson,$network); //再帰
+				array_push($items,$item_parsed_tx);
+			}
+			$tx_layer["layout"] = $items;
+			array_push($parsed_tx,$tx_layer);
+			continue;
+
+		}else if($layer_type === "UnresolvedAddress"){
+			//アドレスに30個の0が続く場合はネームスペースとみなします。
+			if(strpos($tx[$layer["name"]],'000000000000000000000000000000') !== false){
+
+				$network_type = array_filter($catjson, function($cj){
+					return $cj["name"] === "NetworkType";
+				});
+
+				$network_value = array_filter($network_type["values"], function($cj) use($tx){
+					return $cj["name"] === $tx["network"];
+				})["value"];
+
+				$prefix = bin2hex($network_value + 1);
+				$tx[$layer["name"]] =  $prefix + $tx[$layer["name"]];
+			}
+		}else if(isset($catitem["type"]) && $catitem["type"] === "enum"){
+
+			if(strpos($catitem["name"],'Flags') !== false){
+
+				$value = 0;
+				foreach($catitem["values"] as $item_layer){
+
+
+					if(strpos($tx[$layer["name"]],$item_layer["name"]) !== false){
+
+						$value += $item_layer["value"];
+					}
+				}
+				$catitem["value"] = $value;
+			
+			}else if(isset($layer_disposition) &&  strpos($layer_disposition,'array') !== false ){
+				$values = [];
+				foreach($tx[$layer["name"]] as $item){
+
+					$filter_value = array_filter($catitem["values"], function($cj) use($item){
+						return $cj["name"] === $item;
+					})["value"];
+
+					array_push($values,$filter_value);
+				}
+				$tx[$layer["name"]] = $values;
+			}else{
+			
+				$conditions = ["tx" => $tx,"layer_name" => $layer["name"] ];
+				$filter_value = array_filter($catitem["values"], function($cj) use($conditions){
+					return $cj["name"] === $conditions["tx"][$conditions["layer_name"]];
+				})["value"];
+
+				$catitem["value"] = $filter_value;
+			}
+		}
+
+		//サブルーチンにまとめる構想
+		//$parsed_tx = build_layer($layer_disposition,$tx,$layer);
+
+		//layerの配置
+		if(isset($layer_disposition) && strpos($layer_disposition,'array') !== false ){
+
+			$size = 0;
+			if(isset($tx[$layer["size"]])){
+				$size = $tx[$layer["size"]];
+			}
+
+			if($layer_type === "byte"){
+
+				if(isset($layer["element_disposition"])){ //message
+
+					$sub_layout = $layer;
+					$items = [];
+					for($count = 0; $count < $size; $count++){
+						$tx_layer = [];
+						$tx_layer["signedness"] = $layer["element_disposition"]["signedness"];
+						$tx_layer["name"] = "element_disposition";
+						$tx_layer["size"] = $layer["element_disposition"]["size"];
+						$tx_layer["value"] = substr($tx[$layer["name"]],$count * 2, 2);
+						$tx_layer["type"] = $layer_type;
+//						array_push($items,[$tx_layer]);
+						array_push($items,$tx_layer);
+					}
+					$sub_layout["layout"] = $items;
+					array_push($parsed_tx, $sub_layout);
+
+				}else{print_r("not yet");}
+			}else if(isset($tx[$layer["name"]])){
+
+				$sub_layout = $layer;
+				$items = [];
+				foreach($tx[$layer["name"]] as $tx_item){
+
+
+					$tx_layer = array_filter($catjson, function($cj) use($layer_type){
+						return $cj["name"] === $layer_type;
+					});
+
+					$tx_layer["value"] = $tx_item;
+					
+					if($layer_type === "UnresolvedAddress"){
+						//アドレスに30個の0が続く場合はネームスペースとみなします。
+						if(strpos($txItem,'000000000000000000000000000000') !== false){
+
+							$network_type = array_filter($catjson, function($cj){
+								return $cj["name"] === "NetworkType";
+							});
+
+							$network_value = array_filter($network_type["values"], function($cj) use($tx){
+								return $cj["name"] === $tx["network"];
+							})["value"];
+
+							$prefix = bin2hex($network_value + 1);
+							$tx_layer["value"] =  $prefix + $tx_layer["value"];
+						}
+					}			
+//					array_push($items,[$tx_layer]);
+					array_push($items,$tx_layer);
+				}
+				$sub_layout["layout"] = $items;
+				array_push($parsed_tx,$sub_layout);
+
+			}// else{console.log("not yet");}
+		}else{ //reserved またはそれ以外(定義なし)
+
+			$tx_layer = $layer;
+			if(count($catitem) > 0){
+
+				//catjsonのデータを使う
+				if(isset($catitem["signedness"])){
+				$tx_layer["signedness"]	= $catitem["signedness"];
+
+				}
+				if(isset($catitem["size"])){
+				$tx_layer["size"]  = $catitem["size"];
+
+				}
+				if(isset($catitem["type"])){
+				$tx_layer["type"]  = $catitem["type"];
+
+				}
+				if(isset($catitem["value"])){
+				$tx_layer["value"] = $catitem["value"];
+
+				}
+			}
+
+			//txに指定されている場合上書き(enumパラメータは上書きしない)
+			if(isset($layer["name"]) && isset($catitem["type"]) && $catitem["type"] !== "enum"){
+				$tx_layer["value"] = $tx[$layer["name"]];
+			}else{
+				// そのままtxLayerを追加 
+				print_r($layer["name"]);
+			}
+			array_push($parsed_tx,$tx_layer);
+		}
+	}
+
+	$layer_size = array_filter($parsed_tx, function($pf){
+		return $pf["name"] === "size";
+	});
+
+
+	if(isset($layer_size) && isset($layer_size["size"])){
+		$layer_size["value"] = count_size($parsed_tx);
+	}
+
+	print_r($parsed_tx);
+	return $parsed_tx;
+}
+
+function count_size($item,$alignment) {
+
+	$total_size = 0;
+	
+	//レイアウトサイズの取得
+	if(isset($item)  && isset($item["layout"])){
+		foreach( $item["layout"] as $layer){
+			$item_alignment;
+			if(isset($item["alignment"])){
+				$item_alignment = $item["alignment"];
+			}else{
+				$item_alignment = 0;
+			}
+			$total_size += count_size($layer,$item_alignment); //再帰
+		}
+	//レイアウトを構成するレイヤーサイズの取得
+	}else if(is_array($item)){
+		$layout_size = 0;
+		foreach($item as $layout){
+			$layout_size += count_size($layout,$alignment);
+		}		 
+		if(isset($alignment)  && $alignment > 0){
+			$layout_size = floor(($layout_size  + $alignment - 1) / $alignment ) * $alignment;
+		}
+		$total_size += $layout_size;
+	
+	}else{
+		if(isset($item["size"])){
+			$total_size += $item["size"];
+			//console.log(item.name + ":" + item.size);
+		}else{print_r("no size:" + $item["name"]);}
+	}
+	print_r($total_size);
+	return $total_size;
+
+
+//    return 0;
 }
 
 function build_transaction() {
@@ -141,9 +403,7 @@ function update_transaction() {
     return 0;
 }
 
-function count_size() {
-    return 0;
-}
+
 
 function hexlify_transaction() {
     return 0;
